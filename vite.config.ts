59610099634ede585c2fce37053acdccdeeb55cd
ArcticRug25 <wyw82839148@163.com>
2022-11-23 21:46:08 +0800

    feat: add style

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,35 @@
-import { defineConfig } from 'vite'
+import { defineConfig, normalizePath } from 'vite'
 import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer'
 import path from 'path'
+
+// 用 normalizePath 解决 window 下的路径问题
+const variablePath = normalizePath(path.resolve('./src/style/variable.scss'))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   publicDir: path.join(__dirname, 'public'),
   root: path.join(__dirname, 'src'),
-  plugins: [react()]
+  plugins: [react()],
+  // css
+  css: {
+    // 进行 PostCSS 配置
+    postcss: {
+      plugins: [
+        autoprefixer({
+          // 指定目标浏览器
+          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
+        })
+      ]
+    },
+    modules: {
+      generateScopedName: "[name]__[local]__[hash:base64:5]"
+    },
+    preprocessorOptions: {
+      scss: {
+        // additionalData 的内容会在每个 scss 文件的开头自动注入
+        additionalData: `@import "${variablePath}";`
+      }
+    },
+  }
 })
