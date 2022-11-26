87a3a7fdca90ae7c3eeae3530b7361f164b80012
ArcticRug25 <wyw82839148@163.com>
2022-11-26 16:13:44 +0800

    feat: add lint

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig, normalizePath } from 'vite'
 import react from '@vitejs/plugin-react'
 import autoprefixer from 'autoprefixer'
 import path from 'path'
+import viteEslint from 'vite-plugin-eslint'
+import viteStylelint from '@amatlash/vite-plugin-stylelint'
 
 // 用 normalizePath 解决 window 下的路径问题
 const variablePath = normalizePath(path.resolve('./src/style/variable.scss'))
@@ -10,26 +12,33 @@ const variablePath = normalizePath(path.resolve('./src/style/variable.scss'))
 export default defineConfig({
   publicDir: path.join(__dirname, 'public'),
   root: path.join(__dirname, 'src'),
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteEslint(),
+    viteStylelint({
+      // 对某些文件排除检查
+      exclude: /windicss|node_modules/,
+    }),
+  ],
   // css
   css: {
     modules: {
-      generateScopedName: "[name]__[local]__[hash:base64:5]"
+      generateScopedName: '[name]__[local]__[hash:base64:5]',
     },
     preprocessorOptions: {
       scss: {
         // additionalData 的内容会在每个 scss 文件的开头自动注入
-        additionalData: `@import "${variablePath}";`
-      }
+        additionalData: `@import "${variablePath}";`,
+      },
     },
     // 进行 PostCSS 配置
     postcss: {
       plugins: [
         autoprefixer({
           // 指定目标浏览器
-          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
-        })
-      ]
+          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11'],
+        }),
+      ],
     },
-  }
+  },
 })
